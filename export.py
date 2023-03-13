e1f708a999b090c691cb3de88963be25b87e2910
Artanidos <artanidos@gmail.com>
2023-03-13 13:36:19 +0100

    upd

--- export.py
+++ export.py
@@ -1,8 +1,18 @@
+import sys
 from weasyprint import HTML, CSS
 
-with open("E:/SourceCode/PySide6DesktopBookEnglishPaperback/pythongui.html", "r", encoding="utf-8") as f:
-    html = f.read()
+def export(input, output):
+    with open(input, "r", encoding="utf-8") as f:
+        html = f.read()
 
-h = HTML(string=html)
-css = CSS(string='@page { size: A5; margin: 0.7cm;}')
-h.write_pdf("E:/SourceCode/PySide6DesktopBookEnglishPaperback/pythongui.pdf", stylesheets=[css])
+    h = HTML(string=html)
+    css = CSS(string='@page { size: A5; margin: 0.7cm;}')
+    h.write_pdf(output, stylesheets=[css])
+
+
+if __name__ == "__main__":
+    if len(sys.argv) != 3:
+        print("usage: python export.py <input.html> <output.pdf>")
+        sys.exit()
+
+    export(sys.argv[1], sys.argv[2])
