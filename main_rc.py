13af12d42e810dfecb19ca2d0cadb11e3fe23994
Artanidos <japp.olaf@gmail.com>
2023-06-14 02:29:57 +0200

    upd

--- main_rc.py
+++ main_rc.py
@@ -1,281 +1,275 @@
 # Resource object code (Python 3)
 # Created by: object code
-# Created by: The Resource Compiler for Qt version 6.4.2
+# Created by: The Resource Compiler for Qt version 6.5.1
 # WARNING! All changes made in this file will be lost!
 
 from PySide6 import QtCore
 
 qt_resource_data = b"\
-\x00\x00\x10\xb2\
+\x00\x00\x10Y\
 <\
 ?xml version=\x221.\
 0\x22 encoding=\x22UTF\
 -8\x22 standalone=\x22\
-no\x22?>\x0d\x0a<!-- Crea\
-ted with Inkscap\
-e (http://www.in\
-kscape.org/) -->\
-\x0d\x0a\x0d\x0a<svg\x0d\x0a   xml\
-ns:dc=\x22http://pu\
-rl.org/dc/elemen\
-ts/1.1/\x22\x0d\x0a   xml\
-ns:cc=\x22http://cr\
-eativecommons.or\
-g/ns#\x22\x0d\x0a   xmlns\
-:rdf=\x22http://www\
-.w3.org/1999/02/\
-22-rdf-syntax-ns\
-#\x22\x0d\x0a   xmlns:svg\
-=\x22http://www.w3.\
-org/2000/svg\x22\x0d\x0a \
-  xmlns=\x22http://\
-www.w3.org/2000/\
-svg\x22\x0d\x0a   xmlns:s\
-odipodi=\x22http://\
-sodipodi.sourcef\
-orge.net/DTD/sod\
-ipodi-0.dtd\x22\x0d\x0a  \
- xmlns:inkscape=\
-\x22http://www.inks\
-cape.org/namespa\
-ces/inkscape\x22\x0d\x0a \
-  width=\x22128\x22\x0d\x0a \
-  height=\x22128\x22\x0d\x0a\
-   viewBox=\x220 0 \
-33.866666 33.866\
-667\x22\x0d\x0a   version\
-=\x221.1\x22\x0d\x0a   id=\x22s\
-vg8\x22\x0d\x0a   inkscap\
+no\x22?>\x0a<!-- Creat\
+ed with Inkscape\
+ (http://www.ink\
+scape.org/) -->\x0a\
+\x0a<svg\x0a   xmlns:d\
+c=\x22http://purl.o\
+rg/dc/elements/1\
+.1/\x22\x0a   xmlns:cc\
+=\x22http://creativ\
+ecommons.org/ns#\
+\x22\x0a   xmlns:rdf=\x22\
+http://www.w3.or\
+g/1999/02/22-rdf\
+-syntax-ns#\x22\x0a   \
+xmlns:svg=\x22http:\
+//www.w3.org/200\
+0/svg\x22\x0a   xmlns=\
+\x22http://www.w3.o\
+rg/2000/svg\x22\x0a   \
+xmlns:sodipodi=\x22\
+http://sodipodi.\
+sourceforge.net/\
+DTD/sodipodi-0.d\
+td\x22\x0a   xmlns:ink\
+scape=\x22http://ww\
+w.inkscape.org/n\
+amespaces/inksca\
+pe\x22\x0a   width=\x2212\
+8\x22\x0a   height=\x2212\
+8\x22\x0a   viewBox=\x220\
+ 0 33.866666 33.\
+866667\x22\x0a   versi\
+on=\x221.1\x22\x0a   id=\x22\
+svg8\x22\x0a   inkscap\
 e:version=\x220.92.\
-4 (unknown)\x22\x0d\x0a  \
- sodipodi:docnam\
-e=\x22logo.svg\x22\x0d\x0a  \
- inkscape:export\
--filename=\x22/home\
-/art/Sourcecode/\
-Python/EbookCrea\
-tor/images/logo.\
-png\x22\x0d\x0a   inkscap\
-e:export-xdpi=\x229\
-6\x22\x0d\x0a   inkscape:\
-export-ydpi=\x2296\x22\
->\x0d\x0a  <defs\x0d\x0a    \
- id=\x22defs2\x22 />\x0d\x0a\
-  <sodipodi:name\
-dview\x0d\x0a     id=\x22\
-base\x22\x0d\x0a     page\
-color=\x22#ffffff\x22\x0d\
-\x0a     bordercolo\
-r=\x22#666666\x22\x0d\x0a   \
-  borderopacity=\
-\x221.0\x22\x0d\x0a     inks\
-cape:pageopacity\
-=\x220.0\x22\x0d\x0a     ink\
-scape:pageshadow\
-=\x222\x22\x0d\x0a     inksc\
-ape:zoom=\x221.5274\
-747\x22\x0d\x0a     inksc\
-ape:cx=\x22-37.4692\
-34\x22\x0d\x0a     inksca\
+4 (unknown)\x22\x0a   \
+sodipodi:docname\
+=\x22logo.svg\x22\x0a   i\
+nkscape:export-f\
+ilename=\x22/home/a\
+rt/Sourcecode/Py\
+thon/EbookCreato\
+r/images/logo.pn\
+g\x22\x0a   inkscape:e\
+xport-xdpi=\x2296\x22\x0a\
+   inkscape:expo\
+rt-ydpi=\x2296\x22>\x0a  \
+<defs\x0a     id=\x22d\
+efs2\x22 />\x0a  <sodi\
+podi:namedview\x0a \
+    id=\x22base\x22\x0a  \
+   pagecolor=\x22#f\
+fffff\x22\x0a     bord\
+ercolor=\x22#666666\
+\x22\x0a     borderopa\
+city=\x221.0\x22\x0a     \
+inkscape:pageopa\
+city=\x220.0\x22\x0a     \
+inkscape:pagesha\
+dow=\x222\x22\x0a     ink\
+scape:zoom=\x221.52\
+74747\x22\x0a     inks\
+cape:cx=\x22-37.469\
+234\x22\x0a     inksca\
 pe:cy=\x2292.605789\
-\x22\x0d\x0a     inkscape\
-:document-units=\
-\x22mm\x22\x0d\x0a     inksc\
-ape:current-laye\
-r=\x22layer1\x22\x0d\x0a    \
- showgrid=\x22false\
-\x22\x0d\x0a     units=\x22p\
-x\x22\x0d\x0a     inkscap\
-e:window-width=\x22\
-1600\x22\x0d\x0a     inks\
-cape:window-heig\
-ht=\x22832\x22\x0d\x0a     i\
-nkscape:window-x\
-=\x220\x22\x0d\x0a     inksc\
-ape:window-y=\x220\x22\
-\x0d\x0a     inkscape:\
-window-maximized\
-=\x221\x22 />\x0d\x0a  <meta\
-data\x0d\x0a     id=\x22m\
-etadata5\x22>\x0d\x0a    \
-<rdf:RDF>\x0d\x0a     \
- <cc:Work\x0d\x0a     \
-    rdf:about=\x22\x22\
->\x0d\x0a        <dc:f\
-ormat>image/svg+\
-xml</dc:format>\x0d\
+\x22\x0a     inkscape:\
+document-units=\x22\
+mm\x22\x0a     inkscap\
+e:current-layer=\
+\x22layer1\x22\x0a     sh\
+owgrid=\x22false\x22\x0a \
+    units=\x22px\x22\x0a \
+    inkscape:win\
+dow-width=\x221600\x22\
+\x0a     inkscape:w\
+indow-height=\x2283\
+2\x22\x0a     inkscape\
+:window-x=\x220\x22\x0a  \
+   inkscape:wind\
+ow-y=\x220\x22\x0a     in\
+kscape:window-ma\
+ximized=\x221\x22 />\x0a \
+ <metadata\x0a     \
+id=\x22metadata5\x22>\x0a\
+    <rdf:RDF>\x0a  \
+    <cc:Work\x0a   \
+      rdf:about=\
+\x22\x22>\x0a        <dc:\
+format>image/svg\
++xml</dc:format>\
 \x0a        <dc:typ\
-e\x0d\x0a           rd\
-f:resource=\x22http\
-://purl.org/dc/d\
-cmitype/StillIma\
-ge\x22 />\x0d\x0a        \
-<dc:title></dc:t\
-itle>\x0d\x0a      </c\
-c:Work>\x0d\x0a    </r\
-df:RDF>\x0d\x0a  </met\
-adata>\x0d\x0a  <g\x0d\x0a  \
-   inkscape:labe\
-l=\x22Ebene 1\x22\x0d\x0a   \
-  inkscape:group\
-mode=\x22layer\x22\x0d\x0a  \
-   id=\x22layer1\x22\x0d\x0a\
+e\x0a           rdf\
+:resource=\x22http:\
+//purl.org/dc/dc\
+mitype/StillImag\
+e\x22 />\x0a        <d\
+c:title></dc:tit\
+le>\x0a      </cc:W\
+ork>\x0a    </rdf:R\
+DF>\x0a  </metadata\
+>\x0a  <g\x0a     inks\
+cape:label=\x22Eben\
+e 1\x22\x0a     inksca\
+pe:groupmode=\x22la\
+yer\x22\x0a     id=\x22la\
+yer1\x22\x0a     trans\
+form=\x22translate(\
+0,-263.13332)\x22>\x0a\
+    <rect\x0a      \
+ style=\x22fill:#ff\
+ffff;fill-opacit\
+y:1;stroke-width\
+:0.13162293\x22\x0a   \
+    id=\x22rect923\x22\
+\x0a       width=\x223\
+3.866665\x22\x0a      \
+ height=\x2233.8666\
+65\x22\x0a       x=\x228.\
+9009603e-07\x22\x0a   \
+    y=\x22263.13333\
+\x22 />\x0a    <g\x0a    \
+   id=\x22g1462\x22\x0a  \
      transform=\x22\
-translate(0,-263\
-.13332)\x22>\x0d\x0a    <\
-rect\x0d\x0a       sty\
-le=\x22fill:#ffffff\
-;fill-opacity:1;\
-stroke-width:0.1\
-3162293\x22\x0d\x0a      \
- id=\x22rect923\x22\x0d\x0a \
-      width=\x2233.\
-866665\x22\x0d\x0a       \
-height=\x2233.86666\
-5\x22\x0d\x0a       x=\x228.\
-9009603e-07\x22\x0d\x0a  \
-     y=\x22263.1333\
-3\x22 />\x0d\x0a    <g\x0d\x0a \
-      id=\x22g1462\x22\
-\x0d\x0a       transfo\
-rm=\x22matrix(0.538\
-56631,0,0,0.5385\
-6631,0.05936206,\
-138.35192)\x22>\x0d\x0a  \
-    <path\x0d\x0a     \
-    sodipodi:nod\
-etypes=\x22cccccccc\
-ccsccccccccccccc\
-cccccscccc\x22\x0d\x0a   \
-      inkscape:c\
-onnector-curvatu\
-re=\x220\x22\x0d\x0a        \
- id=\x22path845\x22\x0d\x0a \
-        d=\x22m 26.\
-832075,285.73828\
- c -0.32573,-0.0\
-616 -0.681472,-0\
-.2093 -0.976316,\
--0.40513 -0.2101\
-94,-0.1396 -20.5\
-57212,-20.48281 \
--20.7157113,-20.\
-71183 -0.2278125\
-,-0.32918 -0.376\
-2882,-0.69566 -0\
-.4380724,-1.0812\
-9 -0.047531,-0.2\
-9665 -0.019039,-\
-0.82386 0.059059\
-,-1.0932 0.07972\
-,-0.2749 0.26359\
-83,-0.65107 0.42\
-51806,-0.8698 0.\
-1732095,-0.23447\
- 20.3979681,-20.\
-43851 20.6212191\
-,-20.60009 0.315\
-046,-0.22804 0.6\
-70057,-0.37873 1\
-.04813,-0.44495 \
-0.235184,-0.0412\
- 0.647066,-0.046\
- 0.872672,-0.010\
-3 0.386094,0.061\
-3 0.837177,0.251\
-96 1.135569,0.47\
-996 0.08638,0.06\
-6 2.973008,2.936\
-62 6.414738,6.37\
-917 l 6.127131,6\
-.2822 -6.984832,\
-7.09239 -7.11539\
-3,7.11543 -2.367\
-777,-2.36777 -1.\
-901636,-2.37148 \
-4.281464,-4.7439\
-6 4.747603,-4.74\
-765 -2.373807,-2\
-.37381 -2.373807\
-,-2.37381 -7.121\
-454,7.12146 -7.1\
-21454,7.12145 7.\
-121455,7.12146 7\
-.121457,7.12146 \
-9.48947,-9.48946\
- 9.357057,-9.542\
-4 1.68131,1.6055\
-1 c 1.002344,1.0\
-0472 1.585125,1.\
-60684 1.651568,1\
-.70636 0.605317,\
-0.90677 0.609044\
-,2.01417 0.0099,\
-2.93424 -0.08975\
-,0.13783 -2.2145\
-31,2.27618 -10.2\
-87435,10.35313 -\
-6.279432,6.28258\
- -10.246998,10.2\
-3465 -10.364165,\
-10.32364 -0.2331\
-18,0.17708 -0.56\
-4626,0.33901 -0.\
-864562,0.42231 -\
-0.285737,0.0793 \
--0.863007,0.1026\
-2 -1.158538,0.04\
-66 z\x22\x0d\x0a         \
-style=\x22fill:#9fd\
-723;fill-opacity\
-:1;stroke-width:\
-0.02416104\x22 />\x0d\x0a\
-      <text\x0d\x0a   \
-      id=\x22text14\
-39\x22\x0d\x0a         y=\
-\x22276.78156\x22\x0d\x0a   \
-      x=\x2229.1479\
-07\x22\x0d\x0a         st\
-yle=\x22font-style:\
-normal;font-weig\
-ht:normal;font-s\
-ize:39.8285141px\
-;line-height:1.2\
-5;font-family:sa\
-ns-serif;letter-\
-spacing:0px;word\
--spacing:0px;fil\
-l:#333333;fill-o\
-pacity:1;stroke:\
-none;stroke-widt\
-h:0.99571282\x22\x0d\x0a \
+matrix(0.5385663\
+1,0,0,0.53856631\
+,0.05936206,138.\
+35192)\x22>\x0a      <\
+path\x0a         so\
+dipodi:nodetypes\
+=\x22ccccccccccsccc\
+cccccccccccccccs\
+cccc\x22\x0a         i\
+nkscape:connecto\
+r-curvature=\x220\x22\x0a\
+         id=\x22pat\
+h845\x22\x0a         d\
+=\x22m 26.832075,28\
+5.73828 c -0.325\
+73,-0.0616 -0.68\
+1472,-0.2093 -0.\
+976316,-0.40513 \
+-0.210194,-0.139\
+6 -20.557212,-20\
+.48281 -20.71571\
+13,-20.71183 -0.\
+2278125,-0.32918\
+ -0.3762882,-0.6\
+9566 -0.4380724,\
+-1.08129 -0.0475\
+31,-0.29665 -0.0\
+19039,-0.82386 0\
+.059059,-1.0932 \
+0.07972,-0.2749 \
+0.2635983,-0.651\
+07 0.4251806,-0.\
+8698 0.1732095,-\
+0.23447 20.39796\
+81,-20.43851 20.\
+6212191,-20.6000\
+9 0.315046,-0.22\
+804 0.670057,-0.\
+37873 1.04813,-0\
+.44495 0.235184,\
+-0.0412 0.647066\
+,-0.046 0.872672\
+,-0.0103 0.38609\
+4,0.0613 0.83717\
+7,0.25196 1.1355\
+69,0.47996 0.086\
+38,0.066 2.97300\
+8,2.93662 6.4147\
+38,6.37917 l 6.1\
+27131,6.2822 -6.\
+984832,7.09239 -\
+7.115393,7.11543\
+ -2.367777,-2.36\
+777 -1.901636,-2\
+.37148 4.281464,\
+-4.74396 4.74760\
+3,-4.74765 -2.37\
+3807,-2.37381 -2\
+.373807,-2.37381\
+ -7.121454,7.121\
+46 -7.121454,7.1\
+2145 7.121455,7.\
+12146 7.121457,7\
+.12146 9.48947,-\
+9.48946 9.357057\
+,-9.5424 1.68131\
+,1.60551 c 1.002\
+344,1.00472 1.58\
+5125,1.60684 1.6\
+51568,1.70636 0.\
+605317,0.90677 0\
+.609044,2.01417 \
+0.0099,2.93424 -\
+0.08975,0.13783 \
+-2.214531,2.2761\
+8 -10.287435,10.\
+35313 -6.279432,\
+6.28258 -10.2469\
+98,10.23465 -10.\
+364165,10.32364 \
+-0.233118,0.1770\
+8 -0.564626,0.33\
+901 -0.864562,0.\
+42231 -0.285737,\
+0.0793 -0.863007\
+,0.10262 -1.1585\
+38,0.0466 z\x22\x0a   \
+      style=\x22fil\
+l:#9fd723;fill-o\
+pacity:1;stroke-\
+width:0.02416104\
+\x22 />\x0a      <text\
+\x0a         id=\x22te\
+xt1439\x22\x0a        \
+ y=\x22276.78156\x22\x0a \
+        x=\x2229.14\
+7907\x22\x0a         s\
+tyle=\x22font-style\
+:normal;font-wei\
+ght:normal;font-\
+size:39.8285141p\
+x;line-height:1.\
+25;font-family:s\
+ans-serif;letter\
+-spacing:0px;wor\
+d-spacing:0px;fi\
+ll:#333333;fill-\
+opacity:1;stroke\
+:none;stroke-wid\
+th:0.99571282\x22\x0a \
         xml:spac\
 e=\x22preserve\x22><ts\
-pan\x0d\x0a           \
-style=\x22font-styl\
-e:normal;font-va\
-riant:normal;fon\
-t-weight:bold;fo\
-nt-stretch:norma\
-l;font-family:'U\
-RW Bookman L';-i\
-nkscape-font-spe\
-cification:'URW \
-Bookman L Bold';\
-fill:#333333;fil\
-l-opacity:1;stro\
-ke-width:0.99571\
-282\x22\x0d\x0a          \
- y=\x22276.78156\x22\x0d\x0a\
-           x=\x2229\
-.147907\x22\x0d\x0a      \
-     id=\x22tspan14\
-37\x22\x0d\x0a           \
-sodipodi:role=\x22l\
-ine\x22>C</tspan></\
-text>\x0d\x0a    </g>\x0d\
-\x0a  </g>\x0d\x0a</svg>\x0d\
-\x0a\
+pan\x0a           s\
+tyle=\x22font-style\
+:normal;font-var\
+iant:normal;font\
+-weight:bold;fon\
+t-stretch:normal\
+;font-family:'UR\
+W Bookman L';-in\
+kscape-font-spec\
+ification:'URW B\
+ookman L Bold';f\
+ill:#333333;fill\
+-opacity:1;strok\
+e-width:0.995712\
+82\x22\x0a           y\
+=\x22276.78156\x22\x0a   \
+        x=\x2229.14\
+7907\x22\x0a          \
+ id=\x22tspan1437\x22\x0a\
+           sodip\
+odi:role=\x22line\x22>\
+C</tspan></text>\
+\x0a    </g>\x0a  </g>\
+\x0a</svg>\x0a\
 "
 
 qt_resource_name = b"\
@@ -295,7 +289,7 @@ qt_resource_struct = b"\
 \x00\x00\x00\x00\x00\x02\x00\x00\x00\x01\x00\x00\x00\x02\
 \x00\x00\x00\x00\x00\x00\x00\x00\
 \x00\x00\x00\x12\x00\x00\x00\x00\x00\x01\x00\x00\x00\x00\
-\x00\x00\x01\x85\xd2\x1a\x99\xc2\
+\x00\x00\x01\x88\xb71\x5c\xb4\
 "
 
 def qInitResources():
